e733780b1fd0c594ab797af88370753e35c00516
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2018-10-26 23:32:17 -0400

    real initial commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#install vim-plug:
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#backup existing vimrc to this directory
+mv ~/.vimrc ./oldVimrc
+
+#copy plugins to vimrc:
+cat plugs >> ~/.vimrc
+
+#run PlugInstall in vim to install vim-plug plugins, then quit vim
+vim -c "PlugInstall | qa"
+
+#move the rest of the vimrc file, now that the plugins have been installed
+cat config >> ~/.vimrc
