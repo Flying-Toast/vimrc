97558110039f90c4079c7c23f60b8546ca2658ce
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-04-24 20:41:35 -0400

    add .vim filetype

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 mv ~/.vimrc ./oldVimrc
 
 #copy plugins to vimrc:
-cat plugs >> ~/.vimrc
+cat plugs.vim >> ~/.vimrc
 
 #run PlugInstall in vim to install vim-plug plugins, then quit vim
 vim -c "PlugInstall | qa"
 
 #move the rest of the vimrc file, now that the plugins have been installed
-cat config >> ~/.vimrc
+cat config.vim >> ~/.vimrc
