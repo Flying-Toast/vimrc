c1810381bb05b246167f94af40da31e2deece82e
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-07-23 03:12:03 -0400

    script install messagesg

--- user/scripts/install.sh
+++ user/scripts/install.sh
@@ -3,5 +3,9 @@
 cd scripts &> /dev/null
 
 mkdir -p ~/.local/bin
-cp ~/.local/bin/* old/
-cp bin/* ~/.local/bin
+cp ~/.local/bin/* old/ &> /dev/null
+for SCRIPT in bin/*
+do
+	echo "Installing $(basename $SCRIPT) to ~/.local/bin"
+	cp $SCRIPT ~/.local/bin
+done
