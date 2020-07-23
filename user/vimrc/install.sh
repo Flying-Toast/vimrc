13dae976c436dfe61631931376061f50ab1382b6
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-07-23 13:19:57 -0400

    nvim

--- user/vimrc/install.sh
+++ user/vimrc/install.sh
@@ -2,6 +2,14 @@
 
 cd vimrc &> /dev/null
 
+touch ~/.vimrc
+mkdir -p ~/.config/nvim
+touch ~/.config/nvim/init.vim
+mv ~/.config/nvim/init.vim init.vim.old
+echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc" > ~/.config/nvim/init.vim
+
 #install vim-plug:
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
