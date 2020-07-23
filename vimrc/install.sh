9147c1f4cb9fd8f2d68358aa4c3211f9c857113e
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-07-22 22:53:38 -0400

    apkgs

--- vimrc/install.sh
+++ vimrc/install.sh
@@ -3,15 +3,9 @@
 #install vim-plug:
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-#backup existing vimrc to this directory
-mv ~/.vimrc ./oldVimrc
+mv ~/.vimrc ./vimrc.old
 
-#copy plugins to vimrc:
 cat plugs.vim >> ~/.vimrc
-
-#Install and update plugs
 vim -c "PlugInstall | qa"
 vim -c "PlugUpdate | qa"
-
-#move the rest of the vimrc file, now that the plugins have been installed
 cat config.vim >> ~/.vimrc
