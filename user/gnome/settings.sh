045a5435394339e8707be8d651bac66255b76612
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-07-23 01:10:40 -0400

    tweaks

--- user/gnome/settings.sh
+++ user/gnome/settings.sh
@@ -12,3 +12,10 @@ dconf write $TERM_BASE/foreground-color "'#ABB2BF'"
 dconf write $TERM_BASE/background-color "'#282C34'"
 dconf write $TERM_BASE/bold-color "'#ABB2BF'"
 dconf write $TERM_BASE/palette "['rgb(92,99,112)', 'rgb(224,108,117)', 'rgb(152,195,121)', 'rgb(209,154,102)', 'rgb(97,175,239)', 'rgb(198,120,221)', 'rgb(86,182,194)', 'rgb(171,178,191)', 'rgb(92,99,112)', 'rgb(224,108,117)', 'rgb(152,195,121)', 'rgb(209,154,102)', 'rgb(97,175,239)', 'rgb(198,120,221)', 'rgb(86,182,194)', 'rgb(171,178,191)']"
+
+# tweaks
+dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita-dark'"
+dconf write /org/gnome/desktop/background/picture-uri "'file:///usr/share/backgrounds/gnome/Endless-shapes.jpg'"
+dconf write /org/gnome/shell/enabled-extensions "['launch-new-instance@gnome-shell-extensions.gcampax.github.com']"
+dconf write /org/gnome/desktop/input-sources/xkb-options "['lv3:ralt_switch', 'caps:escape']"
+dconf write /org/gnome/desktop/interface/enable-hot-corners false
