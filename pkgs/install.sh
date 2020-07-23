945fba39800183846c5ba5998df186e0801d9d13
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-07-22 23:30:19 -0400

    packages

---
+++ pkgs/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd pkgs &> /dev/null
+
+if [ $(whoami) != "root" ]
+then
+	echo $1 needs root
+	exit 1
+fi
+
+pacman -Sy
+
+for PACKAGE_ADD in $(awk '$1=="+"{print $2}' apkgs)
+do
+	echo "Installing package '$PACKAGE_ADD'"
+	pacman --noconfirm -S $PACKAGE_ADD
+done
+
+for PACKAGE_REMOVE in $(awk '$1=="-"{print $2}' apkgs)
+do
+	echo "Removing package '$PACKAGE_REMOVE'"
+	pacman --noconfirm -Rs $PACKAGE_REMOVE
+done
